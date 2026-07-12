27c1a3e0af0497f9c488f7e686abf1f9fd0d3ebd
Prost3333 <dimabolshoy33@gmail.com>
2026-07-12 13:54:13 +0200

    feat: add new instruments

--- portfolio-service/src/main/java/nlgrandtaskmanager/portfolio_service/repository/PortfolioSnapshotRepository.java
+++ portfolio-service/src/main/java/nlgrandtaskmanager/portfolio_service/repository/PortfolioSnapshotRepository.java
@@ -2,11 +2,12 @@ package nlgrandtaskmanager.portfolio_service.repository;
 
 import nlgrandtaskmanager.portfolio_service.model.PortfolioSnapshot;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
-
+@Repository
 public interface PortfolioSnapshotRepository extends JpaRepository<PortfolioSnapshot, UUID> {
 
     List<PortfolioSnapshot> findByUserIdOrderBySnapshotDateAsc(UUID userId);
